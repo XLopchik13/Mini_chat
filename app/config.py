505de0e7c5606cfac5ad722fabd2e4d963efb281
XLopchik13 <nikita.petrov.it@gmail.com>
2024-10-05 11:43:14 +0300

    Update .gitignore

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str
     model_config = SettingsConfigDict(
-        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env")
+        env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", ".env.example")
     )
 
 
